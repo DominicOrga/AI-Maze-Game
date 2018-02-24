4a4a561c970e9bb41bde27c6988963eccbc6a412
DominicOrga <dominicorga@gmail.com>
2018-02-24 19:37:25 +0800

    Rename LevelManager to NodeGroup

--- Assets/Scripts/NodeGroup.cs
+++ Assets/Scripts/NodeGroup.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelManager : MonoBehaviour {
+public class NodeGroup : MonoBehaviour {
 
     public List<Node> mVariables;
 	
